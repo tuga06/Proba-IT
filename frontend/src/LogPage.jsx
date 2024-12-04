4085255b517e36973192ad224955ef6a7bfec0d0
tudor <gabrieltudor2006@gmail.com>
2024-12-05 01:46:23 +0200

    pagina desktop de login

--- frontend/src/LogPage.jsx
+++ frontend/src/LogPage.jsx
@@ -1,5 +1,7 @@
 import logoH from './assets/Group 89.png';
 import background_d from './assets/Mesa de trabajo 1 1.png';
+import lock from './assets/_x34_78_x2C__Lock_x2C__Password_x2C__Password_Lock_x2C__Secure_Password_1_.png';
+import mail from './assets/mail.png';
 import './index.css';
 
 
@@ -16,11 +18,22 @@ function LogPage() {
                     <div className="LB click">Login</div>
                     <div className="TBRE click underline">Register</div>
                 </div>
-                <div className="box-border h-32 w-32 p-4 border-4 boxDef content-center">
-                    <div className="text-blue">Logeazatecaneefoame</div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className="boxDef">
+                    <div className="boxTitle">Logheăza-te,<br />chiorăie maţele!</div>
+                    <div className="LD">
+                        <div className="impartire">
+                            <img className="imgBack" src={mail}/>
+                            <input className="inputField" placeholder='E-mail'></input>
+                        </div>
+                        <div className="linieAlb"/>
+                        <div className="impartire">
+                            <img className="imgBack" src={lock}/>
+                            <input className="inputField" placeholder='Password' type="password"></input>
+                        </div>
+                        <div className="linieAlb"/>
+                    </div>
+                    <div className="BL">Log in</div>
+                    <div className="FP">Forgot Password</div>
                 </div>
             </div>
         </div>
